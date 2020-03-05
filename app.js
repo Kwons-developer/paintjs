045e29b4e79064fd72d8bc91d5b2eacad4fab10c
Kwon <anstjdrnjs3@naver.com>
2020-03-05 21:56:46 +0900

    updated

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;                // 선의 너비
 
 let painting = false;
 
@@ -6,9 +13,20 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
     const offsetX = event.offsetX;
     const offsetY = event.offsetY;
+    if(!painting){                          //!painting eual painting === false
+        ctx.beginPath();                     //마우스 클릭 안한 상태 일땐 직선을 그리기 위한 좌표 설정을 하는거임.
+        ctx.moveTo(offsetX, offsetY);
+    } else {
+        ctx.lineTo(offsetX, offsetY);
+        ctx.stroke(); 
+    }
 }
 
 function onMouseDown(event){
@@ -18,14 +36,10 @@ function onMouseDown(event){
 function onMouseUp(event){
     painting = false;
 }
-
-function onMouseLeave(event){
-    stopPainting();
-}
-
+ 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
